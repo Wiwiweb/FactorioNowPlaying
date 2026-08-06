5f94299559bb469dc44e568e48bff1a0954e0b6a
Wiwiweb <wiwiweb2@gmail.com>
2026-08-06 13:17:57 -0700

    Fix possible crash

--- control.lua
+++ control.lua
@@ -6,9 +6,14 @@ function print_music_track(player_index)
     local current_track = player.current_music
     if current_track and current_track ~= "" then
       local track_info = mod_data.get(current_track)
-      local track_title = track_info.title and track_info.title or current_track
-      if track_info.variable then
-        track_title = { "", track_title, " ", { "now-playing.variable" } }
+      local track_title
+      if track_info then -- Mod data could be missing this info if ambient-sound was modified by another mod after now-playing
+        track_title = track_info.title and track_info.title or current_track
+        if track_info.variable then
+          track_title = { "", track_title, " ", { "now-playing.variable" } }
+        end
+      else
+        track_title = current_track
       end
       player.print({ "", "[font=default-semibold]", { "now-playing.now-playing" }, "[/font] ", track_title },
         {
