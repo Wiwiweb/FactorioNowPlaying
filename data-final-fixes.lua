8c38990df073cc662c5f027053c1f0369bc99265
Wiwiweb <wiwiweb2@gmail.com>
2026-08-05 21:18:39 -0700

    Give titles to titleless songs

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,16 @@
+local custom_titles = {
+  ["censeqs-discrepancy"] = "cenSeq’s Discrepancy",
+}
+
+-- Turns "my-song-name" into "My Song Name"
+local function titleize(hyphenated_title)
+  local title = hyphenated_title:gsub("-", " ")
+  title = title:gsub("(%a)([%w_']*)", function(first, rest)
+    return first:upper() .. rest
+  end)
+  return title
+end
+
 local ambient_sound_info_table = {}
 for name, sound in pairs(data.raw["ambient-sound"]) do
 
@@ -10,10 +23,21 @@ for name, sound in pairs(data.raw["ambient-sound"]) do
     end
   end
 
+  if custom_titles[name] then
+    sound.title = custom_titles[name]
+  end
+
+  if not sound.title then
+    sound.title = titleize(name)
+  end
+
   ambient_sound_info_table[name] = {
     title = sound.title,
+    type = sound.track_type,
     planet = planet,
+    variable = sound.sound == nil,
   }
+  -- log(name .. " : " .. serpent.line(ambient_sound_info_table[name]))
 end
 
 data:extend({
